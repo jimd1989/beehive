e6c8de002c8193820c2c7cede37663af5453d719
jimd1989 <jamesdalrymple@protonmail.com>
2021-07-08 09:00:41 +1000

    Word module

--- src/Helpers.hs
+++ src/Helpers.hs
@@ -1,7 +1,27 @@
-module Helpers ((◁)) where
+module Helpers ((◁), (◀), (⊙), (●), (◇), fork) where
 
-import Protolude ((.), fmap)
+import Protolude ((.), (<*>), (<=<), (<>), fmap)
+import Control.Applicative (Applicative, liftA2)
+
+fork :: Applicative f ⇒ (a → b → c) → f a → f b → f c
+fork = liftA2
 
 -- Digraph Tl
 f ◁ g = fmap f . g
 infixr 9 ◁
+
+-- Digraph PL
+f ◀ g = f <=< g
+infixr 1 ◀
+
+-- Digraph 0.
+f ⊙ g = fmap f g
+infixl 4 ⊙
+
+-- Digraph 0M
+f ● g = f <*> g
+infixl 4 ●
+
+-- Digraph Dw
+α ◇ ω = α <> ω
+infixr 5 ◇
--- src/Word.hs
+++ src/Word.hs
@@ -1,2 +1,15 @@
 module Word where
 
+import Protolude (Either, Show, (>>=), pure)
+import Control.Applicative (liftA2)
+import Data.Text (Text)
+import Hash (Hash, hash)
+import Helpers (fork)
+
+data Word = Word {
+  wordHash ∷ Hash,
+  wordText ∷ Text
+} deriving Show
+
+word ∷ Text → Either Text Word
+word = fork (liftA2 Word) hash pure
